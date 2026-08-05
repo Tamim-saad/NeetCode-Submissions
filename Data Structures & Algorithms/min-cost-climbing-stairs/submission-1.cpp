0c98c7666d6ee25fd948c7b0eff912f9925faa86
Tamim Hasan Saad <136108639+Tamim-saad@users.noreply.github.com>
2026-08-05 15:33:21 +0600

    Add: min-cost-climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-1.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+ public:
+  int minCostClimbingStairs(vector<int>& cost) {
+    int n = cost.size();
+    vector<int> dp(n + 1, INT_MAX);
+    dp[0] = 0;
+    dp[1] = 0;
+    for (int i = 0; i < n; i++) {
+      if (i + 1 <= n) dp[i + 1] = min(dp[i + 1], cost[i] + dp[i]);
+      if (i + 2 <= n) dp[i + 2] = min(dp[i + 2], cost[i] + dp[i]);
+    }
+    return dp[n];
+  }
+};
